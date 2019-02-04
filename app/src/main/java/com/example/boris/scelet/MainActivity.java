7ade26e0c750b63aa7ea5e1d1581547acb931bc2
Boris Fomin <dreemspoon@gmail.com>
2019-02-04 20:05:05 +0600

    Fixed response http on interface

--- app/src/main/java/com/example/boris/scelet/MainActivity.java
+++ app/src/main/java/com/example/boris/scelet/MainActivity.java
@@ -1,30 +1,38 @@
 package com.example.boris.scelet;
 import com.example.boris.scelet.NeedClass.CheckAutority;
-
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import com.example.boris.scelet.NeedClass.CheckAutority;
 import com.example.boris.scelet.NeedClass.http;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
     CheckAutority fstart = new CheckAutority();
     private static final String TAG = "MyApp";
-    @Override
+
+
+       @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
         Button nextintent = (Button) findViewById(R.id.button3);
+
         nextintent.setOnClickListener(this);
+
         Boolean fstrt = fstart.firststart();
+
         Button gethttp = (Button) findViewById(R.id.gethttp);
+
         gethttp.setOnClickListener(this);
+
+
         if (fstrt == true) {
     startPass(this);
 }
@@ -32,7 +40,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
+       switch (v.getId()) {
+
             case R.id.button3:{
                 startPass(this);
             break;
@@ -41,6 +50,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.gethttp: {
                 http gethttpapi = new http();
                gethttpapi.start();
+
+
+
+
                 break;
             }
         }
--- app/src/main/java/com/example/boris/scelet/NeedClass/http.java
+++ app/src/main/java/com/example/boris/scelet/NeedClass/http.java
@@ -1,6 +1,15 @@
 package com.example.boris.scelet.NeedClass;
 
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Handler;
 import android.util.Log;
+
+
+import com.example.boris.scelet.MainActivity;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -19,55 +28,112 @@ import okhttp3.Response;
 public class http {
 OkHttpClient httpClient = new OkHttpClient();
 String url = "http://89.219.32.195:6080/YT/hs/TradeCity";
+public String responseHttpJsonString ;
+
+
+    //
 //Создание медиатипа для того чтобы парсить строку в json
-public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     private static final String TAG = "MyApp";
-  // Создание объекта Json из строки так , как Requst (Body) не принимает String ;
+// Обявляем интерфейс для его последующей реализации .
+     public interface ResultHandler{
+       void onSuccess(String response);
+       void onFailure(IOException e);
+   }
+    // Создание объекта Json из строки так , как Requst (Body) не принимает String ;
     JSONObject json;
 
     {
         try {
             json = new JSONObject(" {\n" +
-                        " \"Type\": \"Auth\",\n" +
-                        "\t\"User\": \"vIpljYV\",\n" +
-                        "\t\"Pass\": \"vIpljYVKRk\"\n" +
-                        "}");
+                    " \"Type\": \"Auth\",\n" +
+                    "\t\"User\": \"vIpljYV\",\n" +
+                    "\t\"Pass\": \"vIpljYVKRk\"\n" +
+                    "}");
         } catch (JSONException e) {
             e.printStackTrace();
         }
     }
-//Создание объекта RequestBody и добавление в него строки json с указанием кодировки utf-8(JSON - mediatype)
-    RequestBody formBody = (RequestBody) RequestBody.create(JSON,json.toString());
+
+    //Создание объекта RequestBody и добавление в него строки json с указанием кодировки utf-8(JSON - mediatype)
+    RequestBody formBody = (RequestBody) RequestBody.create(JSON, json.toString());
+
     /*RequestBody formBody = new FormBody.Builder()
             .add("Type","Auth")
             .add("User","vIpljYV")
             .add("Pass","vIpljYVKRk")
             .build();
     */
-    public void start(){
-    try {run(); } catch (IOException e){
-        Log.e(TAG,e.toString());}
-}
-void run() throws IOException{
-    Request request = new Request.Builder()
-            .url(url)
-            .addHeader("Authorization","Basic dXBkYXRlOnVwZGF0ZQ==")
-            .post(formBody)
-            .build();
 
-    httpClient.newCall(request).enqueue(new Callback() {
+    // РЕализуем интерфейс для передачи ответа из вызова http
+    ResultHandler resphttp = new ResultHandler() {
         @Override
-        public void onFailure(Call call, IOException e) {
-            call.cancel();
+        public void onSuccess(String response) {
+            responseHttpJsonString = response ;
+        Log.e("ResultHandler",responseHttpJsonString);
         }
 
         @Override
-        public void onResponse(Call call, Response response) throws IOException {
-final String myResponse = response.body().string();
-                       Log.e(TAG,myResponse);
+        public void onFailure(IOException e) {
+
         }
-    });
-}
+    };
 
 
+    public void start() {
+        try {
+            run(resphttp);
+        } catch (IOException e) {
+            Log.e(TAG, e.toString());
+        }
+       }
+
+     void run( final ResultHandler callback) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("Authorization", "Basic dXBkYXRlOnVwZGF0ZQ==")
+                .post(formBody)
+                .build();
+
+        httpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String myResponse = response.body().string();
+
+                Log.e(TAG,myResponse);
+
+callback.onSuccess(myResponse);
+            }
+        });
+
+     }
+
+    class DBHelper extends SQLiteOpenHelper {
+
+        public DBHelper(Context context) {
+            // конструктор суперкласса
+            super(context, "myDB", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            Log.d("CreateDB", "--- onCreate database ---");
+            // создаем таблицу с полями
+            db.execSQL("create table mytable ("
+                    + "id integer primary key autoincrement,"
+                    + "httpresponse text"
+                    + ");");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+    }
 }
+
